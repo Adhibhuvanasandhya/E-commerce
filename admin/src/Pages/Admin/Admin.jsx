270b3959c19123618e449589952dd2495bac1a15
Adhibhuvanasandhya <sandhyaponrajan.l@gmail.com>
2025-01-17 16:51:31 +0530

    File updated

--- admin/src/Pages/Admin/Admin.jsx
+++ admin/src/Pages/Admin/Admin.jsx
@@ -3,7 +3,8 @@ import './Admin.css'
 import Sidebar from '../../Components/Sidebar/Sidebar'
 import { Route, Routes } from 'react-router-dom'
 import AddProduct from '../../Components/AddProduct/AddProduct'
-import ListProduct from '../../Components/ListProduct/ListProduct'
+import ProductList from '../../Components/ProductList/ProductList'
+
 
 const Admin = () => {
   return (
@@ -11,7 +12,7 @@ const Admin = () => {
       <Sidebar/>
       <Routes>
         <Route path='/addproduct' element={<AddProduct/>}></Route>
-        <Route path='/listproduct' element={<ListProduct/>}></Route>
+        <Route path='/listproduct' element={<ProductList/>}></Route>
       </Routes>
       
         
