54e011def4edb4b090119d9ab4cd42c316cc58a6
Adhibhuvanasandhya <sandhyaponrajan.l@gmail.com>
2025-01-17 19:38:36 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -75,7 +75,7 @@ app.post("/upload", upload.single('image'), (req, res) => {
   }
   res.json({
     success: true,
-    image_url: `http://localhost:${port}/images/${req.file.filename}`
+    image_url: `https://e-commerce-backend-mu-sage.vercel.app/images/${req.file.filename}`
   });
 });
 
